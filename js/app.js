dfc45f8ae647b75b83f99a6b261f4a66b0721559
Hayden Turek <hayden@Haydens-MacBook-Pro.local>
2017-03-08 02:25:35 -0600

    refactored

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 'use strict';
 
 $(function() {
-  var defaultWin = false;
+  var loading = false;
   // -------
   var controls = {
+    mag: [],
     tag: null,
     turns: null,
     players: null,
@@ -15,102 +16,80 @@ $(function() {
     gameOver: false,
     gameLoad: [],
     round: null,
-    ties: [],
-    mag: []
+    ties: []
   };
 
+  // LOADER ----------------------
+  $('#loadNew').on('click', function(ev) {
+    ev.preventDefault();
+    if (!loading) {
+      clearGame();
+      loadGame(grabRules());
+    }
+  });
+
   // LISTING CONSTRUCTOR ---------------------
-  function Listing(id, title, price, description) {
+  function Listing (id, title, price, description, url) {
     this.id = id;
     this.title = title;
     this.price = price;
     this.description = description;
+    this.url = url;
   };
-
-  // LOAD GAME ON SUB ---------------
-  $('#loadNew').on('click', function(ev) {
-    ev.preventDefault();
-    clearGame();
-    grabRules();
-    loadGame(controls.tag, controls.turns);
-  });
 
   /////////////////////////////////////////////////////////////////////////////
 
   // INITIALIZES -
-  function initGame() {
-    // FILL CHAMBER --
-    setTimeout(function() {
-      fillChamber();
-    }, 1100);
-    addPlayers(controls.players);
+  function initGame(settings) {
 
-    // ------------------------------------
-    // ---------- ADD PLAY LISTEN ----------
+    reload();
+    addPlayers(settings.players);
+
+    // ---------- PLAY HANDLER ----------
     $('#guessSubmit').on('click', function(ev) {
       ev.preventDefault();
-      if (!controls.donePlaying) {
-        play();
+      var current = parseFloat(settings.round.price);
+      if (!settings.donePlaying) {
+        play(current);
       }
+
     });
-    // ---------
   };
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // CLEARS GAME --
-  function clearGame() {
-    controls.ties = [];
-    controls.gameOver = false;
-    controls.donePlaying = false;
-    controls.scoreboard = {};
-    $('#nameDisplay').text(null);
-    $('#priceViewport').text(null);
-    $('#playerGuesses').empty();
-    $('section.listingDisplay > div').empty();
-    controls.listingObjects = [];
-    controls.gameLoad = [];
-    controls.mag = [];
+  // PLAY ----
+  function play(round) {
+
+    var price = round;
+    var currentIntervals = getIntervals(controls.players, price);
+    var runtInterval = getRunt(currentIntervals);
+    var winningPlayer = currentIntervals.indexOf(runtInterval) + 1;
+
+    updateScore(winningPlayer);
+    checkDefault(controls.turns, controls.scoreboard);
+
+    if (controls.gameOver) {
+      judge();
+    } else {
+      reload();
+    }
+
+    $('#priceViewport').text('PRICE WAS $' + price);
   };
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // GETS RULES ---
-  function grabRules() {
-    controls.tag = $('#tag').val();
-    controls.turns = $('#turns').val();
-    controls.players = $('#players').val();
-  };
+  // DISPLAY --
+  function reload() {
+    console.log('reload() ran');
 
-  /////////////////////////////////////////////////////////////////////////////
-
-  // ADDS PLAYERS ---------
-  function addPlayers(headCount) {
-    for (let i = 1; i <= headCount; i++) {
-      var playerName = prompt(`Enter a name for Player ${i}:`);
-      $('#playerGuesses').append(`<div class=scorecard id=p${i}score>0</div>`);
-      $('#playerGuesses').append(`<label id=label${i} for=player${i}>${playerName}</label>`);
-      $('#playerGuesses').append(`<input type=number class=playerInput id=player${i}>`);
-      controls.scoreboard[`player${i}`] = 0;
-    };
-    $('#playerGuesses').append('<input type=submit id=guessSubmit>');
-  };
-
-  /////////////////////////////////////////////////////////////////////////////
-
-  // LOADS DISPLAY --
-  function fillChamber() {
-    console.log('fillChamber() ran');
-  // ISOLATE -------------------
     controls.round = controls.mag.shift();
     var currentPrice = controls.round.price;
 
-    // DEBUG
+    console.log(controls.round.url);
     console.log(`PRICE = ${currentPrice}`);
-    // console.log(controls.mag);
-    // console.log(controls.gameLoad);
-    // console.log(controls.round);
-    // DEBUG
+
 
     // POPULATE --------------------------
     $('section.listingDisplay > div').empty();
@@ -125,9 +104,9 @@ $(function() {
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // DETERMINE WIN--
-  function nameVictor() {
-    console.log('nameVictor() ran');
+  // JUDGE --------
+  function judge() {
+    console.log('judge() ran');
     var victor;
     var control = 0;
     var currentScore;
@@ -148,9 +127,149 @@ $(function() {
       controls.donePlaying = true;
       return;
     };
-
+    // !!!!!!!!
     $('#nameDisplay').text(`WINNER = ${victor}`);
     controls.donePlaying = true;
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////
+
+  // LOADS GAME ----------------------
+  function loadGame(rules) {
+    loading = true;
+    var ammo = rules.turns;
+    var listingRequest = `https://openapi.etsy.com/v2/listings/active.js?tags=${rules.tag}&limit=99&api_key=${etsyKey}`;
+
+    var getListings = $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url: listingRequest
+    });
+
+    getListings.done(function(data) {
+      var results = data.results;
+      // console.log(results);
+
+      // CONSTRUCT OBJECTS --------
+      results.forEach(function(result) {
+        var listingObject = new Listing (
+          result.listing_id,
+          result.title,
+          result.price,
+          result.description,
+          result.url
+        );
+        controls.listingObjects.push(listingObject);
+      });
+
+      // ESTABLISH RANGE -----------
+      // GET IMAGES ---------------------------
+      //------------------------------------------
+
+      initialize(loadMag(ammo));
+
+      function initialize(ofActive) {
+        var imagePromises = [];
+        ofActive.forEach(function(listing) {
+          var imageRequest = `https://openapi.etsy.com/v2/listings/${listing.id}/images.js?api_key=${etsyKey}`;
+          // WHEN DONE EACH IMAGE --------------
+            callImage(imageRequest, imagePromises).done(function(imageData) {
+            var imageUrl = imageData.results[0].url_570xN;
+            listing.image = imageUrl;
+          });
+        });
+
+        Promise.all(imagePromises).then(function() {
+          loading = false;
+          initGame(rules);
+          imagePromises = [];
+        });
+      };
+
+    });
+  };
+
+  // --------------------------------------------------------------------------
+  /////////////////////////////////////////////////////////////////////////////
+
+  function callImage(location, destination) {
+    var listingImage = $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url: location
+    });
+
+    destination.push(listingImage);
+    return listingImage;
+  };
+
+  //
+
+  function loadMag(clip) {
+    var range = controls.listingObjects.length;
+    // POPULATE GAMELOAD ---------------------
+    while (controls.gameLoad.length < clip) {
+      var randomIndex = Math.floor(Math.random() * range);
+      var randomListing = controls.listingObjects[randomIndex];
+      //------------------------------------------
+      if (!controls.gameLoad.includes(randomListing)) {
+        controls.gameLoad.push(controls.listingObjects[randomIndex]);
+        controls.mag.push(controls.listingObjects[randomIndex]);
+      }
+    }
+    return controls.mag;
+  };
+
+  // --------------------------------------------------------------------------
+  /////////////////////////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////////////////////////
+
+
+  // WIN BY DEFAULT ---------------
+  function checkDefault(turns, scores) {
+    for (let key in scores) {
+      if (turns / scores[key] < 2) {
+        controls.gameOver = true;
+      }
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  // GETS GUESS/PRICE DIFFERENCES -----
+  function getIntervals(headcount, price) {
+    var guess;
+    var intervals = [];
+    var difference;
+    for (let i = 1; i <= headcount; i++) {
+      // might not need parseFloat here...
+      guess = parseFloat($(`#player${i}`).val());
+      if (guess > price) {
+        difference = guess - price;
+        intervals.push(difference);
+      } else if (guess < price) {
+        difference = price - guess;
+        intervals.push(difference);
+      } else {
+        intervals.push(0);
+      }
+    }
+    return intervals;
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  // LEAST DEGREE OF DIFFERENCE
+  function getRunt(intervals) {
+    var runt = intervals[0];
+    for (let i = 1; i <= intervals.length; i++) {
+      if (intervals[i] < runt) {
+        runt = intervals[i];
+      }
+    }
+    return runt;
   };
 
   /////////////////////////////////////////////////////////////////////////////
@@ -180,54 +299,38 @@ $(function() {
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // CHECKS FOR DEFAULT VICTOR-----
-  function checkDefault(turns, scores) {
-    for (let key in scores) {
-      if (turns / scores[key] < 2) {
-        controls.gameOver = true;
-      }
-    }
+  // CLEARS GAME --
+  function clearGame() {
+    controls.ties = [];
+    controls.gameOver = false;
+    controls.donePlaying = false;
+    controls.scoreboard = {};
+    $('#nameDisplay').text(null);
+    $('#priceViewport').text(null);
+    $('#playerGuesses').empty();
+    $('section.listingDisplay > div').empty();
+    controls.listingObjects = [];
+    controls.gameLoad = [];
+    controls.mag = [];
   };
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // -- GETS HOW FAR OFF YOU WERE
-  function getIntervals(headcount, price) {
-    var guess;
-    var intervals = [];
-    var difference;
-    for (let i = 1; i <= headcount; i++) {
-      // might not need parseFloat here...
-      guess = parseFloat($(`#player${i}`).val());
-      if (guess > price) {
-        difference = guess - price;
-        intervals.push(difference);
-      } else if (guess < price) {
-        difference = price - guess;
-        intervals.push(difference);
-      } else {
-        intervals.push(0);
-      }
-    }
-    return intervals;
-  };
-
-  /////////////////////////////////////////////////////////////////////////////
-  // GETS HOW FAR OFF OF LEAST FAR OFF
-  function getRunt(intervals) {
-    var runt = intervals[0];
-    for (let i = 1; i <= intervals.length; i++) {
-      if (intervals[i] < runt) {
-        runt = intervals[i];
-      }
-    }
-    return runt;
+  // GETS RULES ---
+  function grabRules() {
+    controls.tag = $('#tag').val();
+    controls.turns = $('#turns').val();
+    controls.players = $('#players').val();
+    return controls;
   };
 
   /////////////////////////////////////////////////////////////////////////////
 
-  function updateScoreboard(winner) {
+  // DISPLAYS / TRACKS SCORES
+  function updateScore(winner) {
     var newScore;
+    var roundWinner = $(`#label${winner}`).text();
+    console.log(`ROUND WINNER = ${roundWinner}`);
     controls.scoreboard[`player${winner}`]++;
     newScore = controls.scoreboard[`player${winner}`];
     $(`#p${winner}score`).text(newScore);
@@ -235,85 +338,16 @@ $(function() {
 
   /////////////////////////////////////////////////////////////////////////////
 
-  // CHECKS ANSWERS --------------
-  function play() {
-    //make this ting below a param of play()
-    var currentPrice = parseFloat(controls.round.price);
-
-    var currentIntervals = getIntervals(controls.players, currentPrice);
-    var runtInterval = getRunt(currentIntervals);
-    var winningPlayer = currentIntervals.indexOf(runtInterval) + 1;
-
-    updateScoreboard(winningPlayer);
-    checkDefault(controls.turns, controls.scoreboard);
-
-    if (controls.gameOver) {
-      nameVictor();
-    } else {
-      fillChamber();
-    }
-
-    // DEBUG
-    // console.log('Winner off by = ' + runtInterval);
-    // console.log('winning player = ' + winningPlayer);
-    // DEBUG
-    $('#priceViewport').text('PRICE WAS $' + currentPrice);
-  };
-
-  /////////////////////////////////////////////////////////////////////////////
-
-  // LOADS GAME ----------------------
-  function loadGame(tagParam, gameLength) {
-    var listingRequest = `https://openapi.etsy.com/v2/listings/active.js?tags=${tagParam}&limit=99&api_key=${etsyKey}`;
-    // GET LISTINGS -------
-    var getListings = $.ajax({
-      type: 'GET',
-      dataType: 'jsonp',
-      url: listingRequest
-    });
-    // WHEN DONE ---------------
-    getListings.done(function(data) {
-      var results = data.results;
-      // CONSTRUCT OBJECTS --------
-      results.forEach(function(result) {
-        var listingObject = new Listing (
-          result.listing_id,
-          result.title,
-          result.price,
-          result.description
-        );
-        controls.listingObjects.push(listingObject);
-      });
-      // ESTABLISH RANGE -----------
-      controls.range = controls.listingObjects.length;
-      // POPULATE GAMELOAD ---------------------
-      while (controls.gameLoad.length < gameLength) {
-        var randomIndex = Math.floor(Math.random() * controls.range);
-        var randomListing = controls.listingObjects[randomIndex];
-        //------------------------------------------
-        if (!controls.gameLoad.includes(randomListing)) {
-          // console.log(randomIndex);
-          controls.gameLoad.push(controls.listingObjects[randomIndex]);
-          controls.mag.push(controls.listingObjects[randomIndex]);
-        }
-      }
-      // GET IMAGES ---------------------------
-      controls.mag.forEach(function(listing) {
-        var imageRequest = `https://openapi.etsy.com/v2/listings/${listing.id}/images.js?api_key=${etsyKey}`;
-        var getListingImage = $.ajax({
-          type: 'GET',
-          dataType: 'jsonp',
-          url: imageRequest
-        });
-        // WHEN DONE IMAGES -----------------
-        getListingImage.done(function(imageData) {
-          var imageUrl = imageData.results[0].url_570xN;
-          listing.image = imageUrl;
-        });
-      });
-      // INITIALIZE GAME
-      initGame();
-    });
+  // ADDS PLAYERS ---------
+  function addPlayers(headCount) {
+    for (let i = 1; i <= headCount; i++) {
+      var playerName = prompt(`Enter a name for Player ${i}:`);
+      $('#playerGuesses').append(`<div class=scorecard id=p${i}score>0</div>`);
+      $('#playerGuesses').append(`<label id=label${i} for=player${i}>${playerName}</label>`);
+      $('#playerGuesses').append(`<input type=number class=playerInput id=player${i}>`);
+      controls.scoreboard[`player${i}`] = 0;
+    };
+    $('#playerGuesses').append('<input type=submit id=guessSubmit>');
   };
   /////////////////////////////////////////////////////////////////////////////
 });
